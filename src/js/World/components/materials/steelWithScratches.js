2f6b269ff105e7e7454e3984ab0f392f52c1ac0f
birdlaketree <birdlaketree@gmail.com>
2022-05-25 16:14:27 +0200

    refactoring steel material

--- src/js/World/components/materials/steelWithScratches.js
+++ src/js/World/components/materials/steelWithScratches.js
@@ -4,16 +4,16 @@ import {
 } from 'three';
 import { textureHandler } from '../../system/textureHandler';
 
-const shinyNoiseMetal_normalMapUrl       = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Normal.jpg', import.meta.url);
-const shinyNoiseMetal_roughnessMapUrl    = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Roughness.jpg', import.meta.url);
-const shinyNoiseMetal_metalnessMapUrl    = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Metalness.jpg', import.meta.url);
-const shinyNoiseMetal_aoMapUrl           = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_ao.jpg', import.meta.url);
+const normalTexture       = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Normal.jpg', import.meta.url);
+const roughnessTexture    = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Roughness.jpg', import.meta.url);
+const metalnessTexture    = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_Metalness.jpg', import.meta.url);
+const aoTexture           = new URL('/assets/textures/Imperfections_rmpkcg0p_4K_surface_ms/rmpkcg0p_4K_ao.jpg', import.meta.url);
 
 const steelWithSchratches = (color, envmap) => {
-  const aoTexture = textureHandler(shinyNoiseMetal_aoMapUrl);
-  const normalMap = textureHandler(shinyNoiseMetal_normalMapUrl);
-  const roughnessTexture = textureHandler(shinyNoiseMetal_roughnessMapUrl);
-  const metalnessMap = textureHandler(shinyNoiseMetal_metalnessMapUrl);
+  const aoMap = textureHandler(aoTexture);
+  const normalMap = textureHandler(normalTexture);
+  const roughnessMap = textureHandler(roughnessTexture);
+  const metalnessMap = textureHandler(metalnessTexture);
 
   const parameters = {
     envMap: envmap.texture,
@@ -22,12 +22,12 @@ const steelWithSchratches = (color, envmap) => {
     color: color,
     clearcoat: 0,
 
-    aoMap: aoTexture,
+    aoMap: aoMap,
 
     normalMap: normalMap,
 		normalScale: new Vector2( 0.1, 0.1 ),
 
-    roughnessMap: roughnessTexture,
+    roughnessMap: roughnessMap,
     roughness: 0.3,
 
     metalnessMap: metalnessMap,
