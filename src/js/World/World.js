1df82450b400888d8faeed0570b545de829dc9e8
birdlaketree <birdlaketree@gmail.com>
2022-05-25 11:07:48 +0200

    add color util method

--- src/js/World/World.js
+++ src/js/World/World.js
@@ -1,4 +1,5 @@
 import { Color } from "three";
+import { createColor } from "./utils/createColor.js";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Loop } from './system/Loop.js';
 import { createRenderer } from './system/renderer.js';
@@ -52,16 +53,13 @@ class World {
 
     // M1 - blue
 
-    const hue1 = 0.6;
-    const s1 = 1;
-    const l1 = 0.5;
-    const color1 = new Color();
-    color1.setHSL(hue1, s1, l1);
-
-    const defaultColorMaterial1 = defaultColor(color1, envmap);
+    const blueMaterial = defaultColor(
+      createColor(0.6, 1, 0.5),
+      envmap
+    );
 
     for (let i = 0; i < 8; i++) {
-      const cubeItem = cube(defaultColorMaterial1, Math.random() * 1 + 0.2, Math.random() * 1.4 + 0.2, Math.random() * 1 + 0.2);
+      const cubeItem = cube(blueMaterial, Math.random() * 1 + 0.2, Math.random() * 1.4 + 0.2, Math.random() * 1 + 0.2);
       cubeItem.castShadow = true;
       cubeItem.position.x = Math.random() * spreadWidth - spreadWidth/2;
       cubeItem.position.y = Math.random() + 2;
@@ -75,16 +73,13 @@ class World {
 
     // M2 - white
 
-    const hue2 = 0.6;
-    const s2 = 1;
-    const l2 = 1;
-    const color2 = new Color();
-    color2.setHSL(hue2, s2, l2);
-
-    const defaultColorMaterial2 = defaultColor(color2, envmap);
+    const whiteMaterial = defaultColor(
+      createColor(0, 1, 1),
+      envmap
+    );
 
     for (let i = 0; i < 8; i++) {
-      const cubeItem = cube(defaultColorMaterial2, 0.5, 1, 0.5);
+      const cubeItem = cube(whiteMaterial, 0.5, 1, 0.5);
       cubeItem.castShadow = true;
       cubeItem.position.x = Math.random() * spreadWidth - spreadWidth/2;
       cubeItem.position.y = Math.random() + 2;
@@ -98,16 +93,13 @@ class World {
 
     // M3 - black
 
-    const hue3 = 0;
-    const s3 = 0;
-    const l3 = 0.1;
-    const color3 = new Color();
-    color3.setHSL(hue3, s3, l3);
-
-    const defaultColorMaterial3 = defaultColor(color3, envmap);
+    const blackMaterial = defaultColor(
+      createColor(0, 0, 0.06),
+      envmap
+    );
 
     for (let i = 0; i < 8; i++) {
-      const cubeItem = cube(defaultColorMaterial3, Math.random() * 1 + 0.2, Math.random() * 1.4 + 0.2, Math.random() * 1 + 0.2);
+      const cubeItem = cube(blackMaterial, Math.random() * 1 + 0.2, Math.random() * 1.4 + 0.2, Math.random() * 1 + 0.2);
       cubeItem.castShadow = true;
       cubeItem.position.x = Math.random() * spreadWidth - spreadWidth/2;
       cubeItem.position.y = Math.random() + 2;
@@ -121,13 +113,10 @@ class World {
 
     // M4 - frosted
 
-    const hue4 = 0.6;
-    const s4 = 1;
-    const l4 = 1;
-    const color4 = new Color();
-    color4.setHSL(hue4, s4, l4);
-
-    const frostedPlasticMaterial = frostedPlastic(color4, envmap);
+    const frostedPlasticMaterial = frostedPlastic(
+      createColor(0.98, 1, 0.5),
+      envmap
+      );
 
     for (let i = 0; i < 12; i++) {
       const cubeItem = cube(frostedPlasticMaterial, 1, 1, 0.5);
@@ -144,16 +133,13 @@ class World {
 
     // M5
 
-    const hue5 = 0.7;
-    const s5 = 1;
-    const l5 = 0.5;
-    const color5 = new Color();
-    color5.setHSL(hue5, s5, l5);
-
-    const defaultColorMaterial5 = defaultColor(color5, envmap);
+    const magentaMaterial = defaultColor(
+      createColor(0.4, 0.7, 0.4),
+      envmap
+    );
 
     for (let i = 0; i < 8; i++) {
-      const sphereItem = sphere(defaultColorMaterial5, Math.random()/4 + 0.2);
+      const sphereItem = sphere(magentaMaterial, Math.random()/4 + 0.2);
       sphereItem.position.x = Math.random() * spreadWidth - spreadWidth/2;
       sphereItem.position.y = Math.random() + 2;
       sphereItem.position.z = Math.random() * spreadWidth - spreadWidth/2;
